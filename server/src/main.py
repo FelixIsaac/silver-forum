3da7acf8a3c82e1e4b4384c70ddd2269d2131129
mical385 <82591220+mical385@users.noreply.github.com>
2022-06-12 20:55:31 +0800

    Update main.py

--- server/src/main.py
+++ server/src/main.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
-
-import sqlite3,re,base64,os,string,random
+#IT NEEDS THE GOD DAMN POSTGRES SERVER TO RUN THE FILE IS RIGHT THERE JUST READ IT AHHHHHHHHHHHHH
+import psycopg2,re,base64,os,string,random
 import scrypt
 
 
 global cursor,conn
-conn = sqlite3.connect('login.db')
+conn = psycopg2.connect(
+    host="localhost",
+    database="login",
+    user="postgres",
+    password="postgres")
 cursor = conn.cursor()
-if not cursor.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()==[('USERS',)]:
-    conn.execute('''CREATE TABLE USERS
-             (ISADMIN       INT     NOT NULL,
-             USERNAME       TEXT    NOT NULL,
-             PASSWORD       TEXT     NOT NULL,
-             EMAIL          TEXT    NOT NULL);
+try:
+    cursor.execute("""SELECT EXISTS (
+    SELECT FROM 
+        pg_tables
+    WHERE 
+        schemaname = 'public' AND 
+        tablename  = 'login'
+    );
+    """)
+    print('yes')
+except:
+    print('no')
+    conn.rollback()
+    cursor.execute('''
+    CREATE TABLE USERS (
+        ISADMIN       INTEGER     NOT NULL,
+        USERNAME       TEXT    NOT NULL,
+        PASSWORD       TEXT     NOT NULL,
+        EMAIL          TEXT    NOT NULL
+             );
             ''')
     conn.commit()
  
@@ -24,25 +42,32 @@ def hash_password(password):
 
 def verify(username, password):
     result=lookup(username)
-    cursor.execute("""SELECT PASSWORD FROM "users" WHERE "username" = "%s";"""%(username))
-    temp=cursor.fetchone()
-    if temp:
-        for i in temp:
-            hashed_password=i
-            break
-    #print(hashed_password)
-    hashed_password=base64.b64decode(hashed_password)
-    #print(hashed_password)
-    try:
-        scrypt.decrypt(hashed_password, password, 0.5)
-        return True
-    except scrypt.error:
+    if result:
+        cursor.execute("""SELECT PASSWORD FROM "users" WHERE "username" = "%s";"""%(username))
+        temp=cursor.fetchone()
+        if temp:
+            for i in temp:
+                hashed_password=i
+                break
+        #print(hashed_password)
+        hashed_password=base64.b64decode(hashed_password)
+        #print(hashed_password)
+        try:
+            scrypt.decrypt(hashed_password, password, 0.5)
+            return True
+        except scrypt.error:
+            return False
+    else:
         return False
 
 def lookup(username):
-    cursor.execute("""SELECT * FROM "users" WHERE "username" = "%s";"""%(username))
-    result = cursor.fetchone()
-    return result
+    try:
+        cursor.execute("""SELECT * FROM users WHERE USERNAME = "%s";"""%(username))
+        result = cursor.fetchone()
+        return result
+    except:
+        conn.rollback()
+        return False
 
 def validate(username,password,email):
     if not (20>=len(username) and 3<=len(username)):
@@ -70,8 +95,9 @@ def add(isadmin,username,password,email):
         temp=hash_password(password)
         #print(temp)
         if isadmin==1:
-            cursor.execute("""INSERT INTO USERS (ISADMIN,USERNAME,PASSWORD,EMAIL) VALUES ("%s","%s","%s","%s");"""%(1,username,temp,email))
+            cursor.execute("""INSERT INTO USERS (ISADMIN,USERNAME,PASSWORD,EMAIL) VALUES ("%s","%s","%s","%s");"""%(0,username,temp,email))
         conn.commit()
+        print('done')
     else:
         print('ERORR:',result[1])
 
@@ -79,28 +105,28 @@ def delete(username):
     cursor.execute("""DELETE FROM USERS WHERE USERNAME = "%s";"""%(username))
     conn.commit()
 
-add('ewe','123456789','ee@a.com')
+add(0,'ewe','123456789','ee@a.com')
 
 #tests for username
-#dd('ee','123456789','ee@aaa.com')
+add(0,'ee','123456789','ee@aaa.com')
 #tests for password
-#add('eaa','12345678','ee@aaa.com')
+add(0,'eaa','12345678','ee@aaa.com')
 #test for email
-#add('esa','123456789','eeaaa.com')
+add(0,'esa','123456789','eeaaa.com')
 
-#print(lookup('ewe'))
+print(lookup('ewe'))
 
-#if verify('ewe','123456789'):
-#    print('corect password')
-#else:
-#    print('wrong password')
+if verify('ewe','123456789'):
+    print('corect password')
+else:
+    print('wrong password')
 
-#if verify('ewe','123456789000000000'):
-#    print('corect password')
-#else:
-#    print('wrong password')
+if verify('ewe','123456789000000000'):
+    print('corect password')
+else:
+    print('wrong password')
 
-#print(lookup('ewe'))
+
 conn.close()
 #for name in cursor.execute('select * from "users" where "username" = "paul";'):
 #    print(name)
